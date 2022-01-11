1d6f569f5185c432f019b0ff5bf7a8c275b965fa
djibadiawara <djiba.diawara97@gmail.com>
2022-01-11 13:08:17 -0500

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaHv4Kh7QnhnpujJBsc4cjMeM8uTwOdeY",
+  authDomain: "houzr-app.firebaseapp.com",
+  projectId: "houzr-app",
+  storageBucket: "houzr-app.appspot.com",
+  messagingSenderId: "846397685594",
+  appId: "1:846397685594:web:e4d3ec02f895fdbc1c3d63"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
