1e7ebc58b09058193f4e3b1e162c21f63db6d077
djibadiawara <djiba.diawara97@gmail.com>
2022-01-12 13:42:16 -0500

    PrivateRoute component and useAuthStatus hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -11,18 +13,18 @@ function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path='/' element={<Explore />} />
+      <Routes>
+      <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          
-
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
-          
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
-          </Routes>
-           {/* NavBar */}
+        </Routes>
+        <Navbar />
           </Router>
           <ToastContainer/>
     </>
