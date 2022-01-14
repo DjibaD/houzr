febfdb889e6e225a254e01ff90edc58085e1b636
djibadiawara <djiba.diawara97@gmail.com>
2022-01-14 14:47:53 -0500

    Save listing to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -6,6 +6,7 @@ import {
     uploadBytesResumable,
     getDownloadURL,
   } from 'firebase/storage'
+  import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
   import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
@@ -160,10 +161,23 @@ function CreateListing() {
         toast.error('Images not uploaded')
         return
       })
+      
+      const formDataCopy = {
+        ...formData,
+        imgUrls,
+        geolocation,
+        timestamp: serverTimestamp(),
+      }
   
-      console.log(imgUrls)
-
-    setLoading(false)
+      delete formDataCopy.images
+      delete formDataCopy.address
+      location && (formDataCopy.location = location)
+      !formDataCopy.offer && delete formDataCopy.discountedPrice
+      
+      const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+      setLoading(false)
+      toast.success('Listing saved')
+      navigate(`/category/${formDataCopy.type}/${docRef.id}`)
   }
 
   const onMutate = (e) => {
