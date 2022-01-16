64072aa253341f756bfc70fa8aae2637e972e6ad
djibadiawara <djiba.diawara97@gmail.com>
2022-01-16 12:19:16 -0500

    Fixed location issue

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -112,7 +112,7 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location = address
+      
     }
 
     // Store image in firebase
@@ -168,10 +168,10 @@ function CreateListing() {
         geolocation,
         timestamp: serverTimestamp(),
       }
-  
+
+      formDataCopy.location = address
       delete formDataCopy.images
       delete formDataCopy.address
-      location && (formDataCopy.location = location)
       !formDataCopy.offer && delete formDataCopy.discountedPrice
       
       const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
